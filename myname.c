95447012c45903c14e9c097b3801050f78e1ff6c
Md-Mehedi-Hasan-Arzu <mharzu10@gmail.com>
2025-10-30 00:48:22 +0600

    only few lines

--- myname.c
+++ myname.c
@@ -1,13 +1,3 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Md. Mehedi Hasan Arzu\n");
-    return 0;
-}
-
 
 //https://docs.github.com/en/get-started/git-basics/
 
-
-//here you can find basic git commands
